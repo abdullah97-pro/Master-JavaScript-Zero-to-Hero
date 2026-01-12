40b746ae95c46088e57e9471f5e9f20b593c1d9a
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-12 09:48:02 +0430

    first js code

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ btn.textContent="Click";
 
 btn.addEventListener('click',() => {
     const div = document.createElement('div');
-    div.textContent = "Hello this is js div";
+    const h1 = document.createElement('h1');
+
+    h1.textContent = "This is my first heading by js";
+
+    div.appendChild(h1);
     bdy.appendChild(div);
 })
