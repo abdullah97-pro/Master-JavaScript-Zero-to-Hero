53f015d73b9df7bf6e14e218e186066deef8de55
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-21 09:59:01 +0430

    variables lectures

--- js/script.js
+++ js/script.js
@@ -37,3 +37,28 @@
 // // let x = 12;
 // const x = 12;
 
+// "use strict";
+// var x = 18; //global
+
+// function show() {
+//     console.log(x);
+//     var y = 19; //local
+//     console.log(y);
+// }
+
+// show()
+// console.log(x);
+// console.log(y);
+
+let x = 12;
+if (x == 12) {
+    let y = 1;
+    var z = 2;
+    const pi = 3.14;
+    console.log(y+z);
+}
+
+console.log(y);
+console.log(pi);
+
+
