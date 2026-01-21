f911272ff52ef5e7a571028f9da6ebd1d08e4457
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-21 09:31:29 +0430

    Hoisting lectures

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
-// var name = 14;
-// var name = "Fanaven";
+// // var name = 14;
+// // var name = "Fanaven";
 
-// name = "Abdullah";
+// // name = "Abdullah";
 
-// let x = 14;
-// // let x = 19;
-// x = 21;
-// {
-//     let x = 15;
-// }
+// // let x = 14;
+// // // let x = 19;
+// // x = 21;
+// // {
+// //     let x = 15;
+// // }
 
-// let laptop = "Laptop";
-// laptop = "mini Laptop";
+// // let laptop = "Laptop";
+// // laptop = "mini Laptop";
 
-// {
-//     let laptop = "Laptop";
-//     {
-// let laptop = "Laptop";
-// {
-// let laptop = "Laptop";
+// // {
+// //     let laptop = "Laptop";
+// //     {
+// // let laptop = "Laptop";
+// // {
+// // let laptop = "Laptop";
 
-// }
-//     }
-// }
+// // }
+// //     }
+// // }
 
 
-let x = 12;
-// let x = 15;
-x = 15;
+// let x = 12;
+// // let x = 15;
+// x = 15;
+
+// console.log(x);
+
+// hoisting
 
 console.log(x);
+var x;
